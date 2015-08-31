171dabc4b0f7cba1215b32a50cac2c508ffdb39d
Greg Patrick <greg@orderwon.com>
2015-08-31 19:50:11 -0400

    Initial Commit

---
+++ MagicSquare/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MagicSquare
+//
+//  Created by Greg Patrick on 8/31/15.
+//  Copyright (c) 2015 Thinkful. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
