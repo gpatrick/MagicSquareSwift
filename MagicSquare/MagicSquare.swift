86943e1a4aa094fa3607018b9f8ead8ecb359f8b
Greg Patrick <greg@orderwon.com>
2015-08-31 21:10:12 -0400

    Added method to place first number and placeholder for next numbers

--- MagicSquare/MagicSquare.swift
+++ MagicSquare/MagicSquare.swift
@@ -10,8 +10,34 @@ import Foundation
 
 class MagicSquare: Printable{
     var matrix: [[Int]]
+    var nextNumber: Int
+    var n: Int
+    
     init(dimension: Int){
         matrix = [[Int]](count: dimension, repeatedValue: [Int](count: dimension, repeatedValue: Int()))
+        nextNumber = 1
+        n = dimension
+        computeValues()
+    }
+    
+    func addFirstNumber(){
+        let middle = n / 2
+        matrix[0][middle] = nextNumber
+        nextNumber++
+    }
+    
+    func addNextNumber(){
+        //Figure out next location
+        //Place next number there
+        //Increment next number
+    }
+    
+    func computeValues(){
+        addFirstNumber()
+        let maxNumber = n * n
+        for i in 1 ... maxNumber {
+            addNextNumber()
+        }
     }
     
     var description: String{
